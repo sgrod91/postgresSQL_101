d3f60cf375e343c566284194af6d063f25874596
Steven Rodriguez <sgrod91@gmail.com>
2017-03-27 15:42:52 -0400

    Exercises 2

--- restaurant_db.sql
+++ restaurant_db.sql
@@ -19,3 +19,31 @@ insert into restaurant values
   (default, 'The Collenade', 16, 4, 'Southern', 'Chicken Tender Salad', TRUE, '2017-03-25'),
   (default, 'Gu''s Dumplings', 10, 4, 'Chinese', 'Chicken Fried Rice', TRUE, '2017-03-26'),
   (default, 'Community', 6, 4, 'BBQ', 'Brisket Sandwich', TRUE, '2017-02-27');
+
+--list restaurants by closest distance--
+select * from restaurant order by distance;
+
+--list top 2 restaurants by distance--
+select * from restaurant order by distance limit 2;
+
+--list top 2 restaurants by stars--
+select * from restaurant order by stars limit 2;
+
+--list the top two restaurants where the distance is less than 2 miles
+select * from restaurant where distance <= 2 order by stars desc limit 2;
+
+--count the number of restaurants--
+select count(*) from restaurant;
+
+--count the number of restaurants by category--
+select category, count(*) from restaurant group by category;
+
+--get avg stars per restaurant by category--
+  select category,
+  	avg(stars) as total_stars
+  from restaurant group by category;
+
+--get max stars per restaurant by category--
+  select category,
+  	max(stars) as total_stars
+  from restaurant group by category;
